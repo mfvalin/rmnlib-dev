5bbdf49952c5db0c87ad3303135ad5b6a73d026e
M. Valin <mfvalin@gmail.com>
2020-07-19 20:32:38 -0400

    demo shared library

---
+++ primitives/libdemo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int Demo1(int a)
+{
+//     printf("Demo1 : a = %d\n",a);
+    return(a);
+}
+float Demo2(int a, float b)
+{
+//     printf("Demo2 : a = %d, b = %f\n",a, b);
+    return (b+a);
+}

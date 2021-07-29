636a43dd6993fbf6e7ffcb527b8556170c4d4f77
M. Valin <mfvalin@gmail.com>
2021-07-29 17:40:44 -0400

    added license

--- fstd98/c_fstd98_mod.F90
+++ fstd98/c_fstd98_mod.F90
@@ -1,3 +1,18 @@
+! Copyright (C) 2021  Environnement et Changement climatique Canada
+!
+! This is free software; you can redistribute it and/or
+! modify it under the terms of the GNU Lesser General Public
+! License as published by the Free Software Foundation,
+! version 2.1 of the License.
+!
+! This software is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+! Lesser General Public License for more details.
+!
+! Author:
+!     M. Valin,   Recherche en Prevision Numerique, 2021
+!
 module c_fstd98_mod
   use ISO_C_BINDING
   implicit none
